d811bdecdf53f5f6a8cc93963b5a7ca981b0939d
Isard Botha <isard.botha@gmail.com>
2023-03-14 23:20:19 +0000

    Various fixes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -437,7 +437,6 @@ return require('packer').startup(function(use)
         'ahmedkhalf/project.nvim',
         config = function ()
             require'project_nvim'.setup {
-                detection_methods = {'pattern'}
             }
         end
     }
