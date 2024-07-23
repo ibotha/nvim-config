e20f57f1885fa88ae33b74994148c54464476abe
Isard Botha <isard.botha@gmail.com>
2024-07-23 10:36:04 +0100

    Overhaul

--- init.lua
+++ init.lua
@@ -1,24 +1,2 @@
--- disable netrw at the very start of your init.lua (strongly advised)
-require('options')
-local plugins = require('plugins')
-require('keybinds')
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-if (vim.g.vscode) then
-else
-	require("lazy").setup(plugins, {})
-end
+require("options")
+require("config.lazy")
